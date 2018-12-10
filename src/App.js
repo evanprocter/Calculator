f833cc11048d436bf5fa16bd91d23a628f802079
Evan Procter <evanprocter@gmail.com>
2018-12-10 14:08:13 -0500

    add calculate operations to app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
+import Buttons from './Buttons';
+import Display from './Display';
+
+
 
 class App extends Component {
   constructor() {
     super()
     this.state = { operations: [] }
   }
+
+  calculateOperations = () => {
+    let result = this.state.operations.join('')
+    if(result) {
+      result = math.eval(result)
+      result = math.format(result, { precision: 14 })
+      result = String(result)
+      this.setState({
+        operations: [result],
+      })
+    }
+  }
   
   render() {
     return (
       <div className="App">
-
+         
       </div>
     );
   }
